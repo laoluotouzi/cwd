ecbdedabafa42b267a8d6a336739f55572c934c9
anghunk <anghunk@gmail.com>
2026-01-23 20:21:05 +0800

    docs: 更新管理面板文档和导航链接
    - 修正博主标签功能的描述，明确标签文字为可选填项
    - 更新导航栏中“快速开始”的链接指向

--- docs/.vitepress/configs/nav.js
+++ docs/.vitepress/configs/nav.js
@@ -1,6 +1,6 @@
 export default [
 	{ text: '首页', link: '/' },
-	{ text: '快速开始', link: '/guide/backend-config' },
+	{ text: '快速开始', link: '/guide/getting-started' },
 	{ text: 'API 文档', link: '/api/overview' },
 	{ text: '管理后台', link: 'https://cwd.zishu.me' },
 ];
